27328e31bebaa4d9201bb205edc833793b1247e3
sreedhar <sreedhar@classroom>
2018-12-16 10:32:53 +0530

    new

---
+++ CRM_automation/src/main/java/org/qaplanet/lib/classA.java
@@ -0,0 +1,17 @@
+package org.qaplanet.lib;
+
+public class classA 
+{
+	public void method1()
+	{
+		System.out.println("method1");
+	}
+	public void method2()
+	{
+		System.out.println("method2");
+	}
+	public void method3()
+	{
+		System.out.println("method3");
+	}
+}
---
+++ CRM_automation/src/main/java/org/qaplanet/lib/classb.java
@@ -0,0 +1,12 @@
+package org.qaplanet.lib;
+
+public class classb {
+	public void method1()
+	{
+		System.out.println("method1");
+	}
+	public void method2()
+	{
+		System.out.println("method2");
+	}
+}
